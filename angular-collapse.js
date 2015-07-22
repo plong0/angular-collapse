a7b218e757ac3b9b6a2025578542f16075ad4506
Pat Long <plong00@gmail.com>
2015-07-22 12:08:35 -0700

    basic vertical collapse animations

--- angular-collapse.js
+++ angular-collapse.js
@@ -1,16 +1,42 @@
-angular.module('angular-collapse', ['ngAnimate'])
+angular.module('angular-collapse', [])
 .directive('ngCollapse', ['$animate', function($animate){
 	return {
 		link: function(scope, element, attrs){
 
+			function expand(){
+				element.removeClass('collapse')
+				  .addClass('collapsing')
+				  .attr('aria-expanded', true)
+				  .attr('aria-hidden', false);
+
+			    var animate = { to: {} };
+
+			    animate.to.height = element[0].scrollHeight + 'px';
+
+				$animate.addClass(element, 'in', animate).then(expandDone);
+			}
+
+			function expandDone(){
+				var css = {};
+				css.height =  'auto';
+
+				element.removeClass('collapsing');
+				element.css(css);
+			}
+
+
 			function collapse(){
-				console.log('collapsing...');
+				var css = {};
+				var animate = { to: {} };
+				
+				css.height = element[0].scrollHeight + 'px';
+				animate.to.height = '0';
 
 				element
 				  // IMPORTANT: The height must be set before adding "collapsing" class.
 				  // Otherwise, the browser attempts to animate from height 0 (in
 				  // collapsing class) to the given height here.
-				  .css({height:element[0].scrollHeight + 'px'})
+				  .css(css)
 				  // initially all panel collapse have the collapse class, this removal
 				  // prevents the animation from jumping to collapsed state
 				  .removeClass('collapse')
@@ -18,34 +44,19 @@ angular.module('angular-collapse', ['ngAnimate'])
 				  .attr('aria-expanded', false)
 				  .attr('aria-hidden', true);
 
-
-				collapseDone();
+				$animate.removeClass(element, 'in', animate).then(collapseDone);
 			}
 
 			function collapseDone(){
+				var css = {};
+				css.height = 0;
 
-				element.css({height: 0});
+				element.css(css);
 				element.removeClass('collapsing');
 				element.addClass('collapse');
-
 			}
 
-			function expand(){
-				console.log('expanding...');
-
-				element.removeClass('collapse')
-				  .addClass('collapsing')
-				  .attr('aria-expanded', true)
-				  .attr('aria-hidden', false);
-
-				  expandDone();
-			}
-
-			function expandDone(){
-
-				element.removeClass('collapsing');
-				element.css({height: 'auto'});
-			}
+			
 
 			scope.$watch(attrs.ngCollapse, function(shouldCollapse){
 				if (shouldCollapse) {
